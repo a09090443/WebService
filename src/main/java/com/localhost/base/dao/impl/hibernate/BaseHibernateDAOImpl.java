b1579996e034d690686648217012ab204ad3f3fe
zipe <zipe@zipe>
2016-11-18 11:37:30 +0800

    Add genHql method.

--- src/main/java/com/localhost/base/dao/impl/hibernate/BaseHibernateDAOImpl.java
+++ src/main/java/com/localhost/base/dao/impl/hibernate/BaseHibernateDAOImpl.java
@@ -3,12 +3,14 @@ package com.localhost.base.dao.impl.hibernate;
 import java.beans.BeanInfo;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.beanutils.PropertyUtils;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.localhost.base.dao.BaseHibernateDAO;
@@ -16,7 +18,9 @@ import com.localhost.base.dao.BaseHibernateDAO;
 @Transactional
 public abstract class BaseHibernateDAOImpl<T extends Object> implements BaseHibernateDAO<T> {
 	private Class<T> clazz;
-
+	private String entityName;
+	
+	@Autowired
 	SessionFactory sessionFactory;
 
 	public void setClazz(final Class<T> clazzToSet) {
@@ -53,29 +57,12 @@ public abstract class BaseHibernateDAOImpl<T extends Object> implements BaseHibe
 	}
 
 	@Transactional(readOnly = false)
-	public void update(final T entity, Map<String, Object> args) throws Exception {
-		BeanInfo beanInfo = Introspector.getBeanInfo(entity.getClass());
-		PropertyDescriptor[] descriptors = beanInfo.getPropertyDescriptors();
-		String entityName = entity.getClass().getSimpleName();
-		StringBuilder hqlStr = new StringBuilder();
-		hqlStr.append("UPDATE " + entityName + " " + entityName + " SET ");
-		for (int i = 0; i < descriptors.length; i++) {
-			String propName = descriptors[i].getName();
-			Class<?> propType = descriptors[i].getPropertyType();
-			Object value = PropertyUtils.getProperty(entity, propName);
-			if (!propName.equals("class") && null != value) {
-				if (propType.getSimpleName().equals("Integer") || propType.getSimpleName().equals("int")) {
-					hqlStr.append(entityName + "." + propName + "=" + value + "");
-				} else if (propType.getSimpleName().equals("String")) {
-					hqlStr.append(entityName + "." + propName + "=" + "'" + value + "'");
-				}
-				hqlStr.append(",");
-			}
-		}
-		hqlStr = hqlStr.deleteCharAt(hqlStr.length() - 1);
-		hqlStr = hqlStr.append(" WHERE User.userId=:userId AND User.account=:account");
-
-		getCurrentSession().createQuery(hqlStr.toString()).setProperties(args).executeUpdate();
+	public void update(final T entity, Map<String, Object> argMap) throws Exception {
+		entityName = entity.getClass().getSimpleName();
+		String hqlStr;
+		hqlStr = "UPDATE " + entityName + " " + entityName + " SET ";
+		hqlStr += this.genHql(entity, argMap, "");
+		getCurrentSession().createQuery(hqlStr).setProperties(argMap).executeUpdate();
 	}
 
 	@Transactional(readOnly = false)
@@ -104,5 +91,60 @@ public abstract class BaseHibernateDAOImpl<T extends Object> implements BaseHibe
 
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
+	}
+	
+	private String genHql(Object entity, Map<String, Object> argMap, String hqlStr) throws Exception {
+		String whereStr = "";
+		BeanInfo beanInfo = Introspector.getBeanInfo(entity.getClass());
+		Integer entityLen = entity.getClass().getSimpleName().length();
+		Boolean compareEntityName = entity.getClass().getSimpleName().substring(entityLen - 2, entityLen).equals("Id");
+		PropertyDescriptor[] descriptors = beanInfo.getPropertyDescriptors();
+		for (int i = 0; i < descriptors.length; i++) {
+			String propName = descriptors[i].getName();
+			Class<?> propType = descriptors[i].getPropertyType();
+			Object value = PropertyUtils.getProperty(entity, propName);
+			if ((!propName.equals("class") && null != value) || propName.equals("id")) {
+				if (propType.getSimpleName().equals("Integer") || propType.getSimpleName().equals("int")) {
+					if (compareEntityName) {
+						hqlStr += entityName + ".id." + propName + "=" + value + "";
+					} else {
+						hqlStr += entityName + "." + propName + "=" + value + "";
+					}
+				} else if (propType.getSimpleName().equals("String")) {
+					if (compareEntityName) {
+						hqlStr += entityName + ".id." + propName + "=" + "'" + value + "'";
+					} else {
+						hqlStr += entityName + "." + propName + "=" + "'" + value + "'";
+					}
+				} else if (propName.equals("id")) {
+					if(null != value){
+						hqlStr = this.genHql(value, null, hqlStr);
+					}
+					Class<?> c = null;
+					c = Class.forName(propType.getName());
+					Field[] fields = c.getDeclaredFields();
+					for (Field f : fields) {
+						f.setAccessible(true);
+					}
+					for (Field f : fields) {
+						String field = f.toString().substring(f.toString().lastIndexOf(".") + 1);
+						if (null != argMap.get(field)) {
+							whereStr += entityName + ".id." + field + "=:" + field + " AND ";
+						}
+					}
+				}
+				if(null != value){
+					hqlStr += ",";
+				}
+				if (null != argMap && null != argMap.get(propName)) {
+					whereStr += entityName + "." + propName + "=:" + propName + " AND ";
+				}
+			}
+		}
+		if (!whereStr.isEmpty()) {
+			whereStr = " WHERE " + whereStr.substring(0, whereStr.length() - 5);
+		}
+		hqlStr = hqlStr.substring(0, hqlStr.length() - 1) + whereStr;
+		return hqlStr;
 	}
 }
